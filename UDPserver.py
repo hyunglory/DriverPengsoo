8a6e028e8e77906e6506bade22f0c2c0607bf8af
glory <hyunglory86@gmail.com>
2020-11-11 12:27:44 +0900

    머지 에러 해결

--- UDPserver.py
+++ UDPserver.py
@@ -40,22 +40,13 @@ class server:
 
     # 분기문
     def controllData(self, data):  
-        command = Command()
-        device = Device()
-        db = MongoDB()      
         start_index = data.find('[')
         end_index = data.find("]")
-<<<<<<< HEAD
 
         data = data[start_index:]        
         who = data[:end_index]
         cmd = data[end_index+1:]
         print("who:",who)
-=======
-        who = data[:end_index+1]
-        cmd = data[end_index+1:]
-        print("who:", who)
->>>>>>> 1c7f4a5aeea320f75b04f971e069a4d7eeb884e1
         print("cmd:", cmd)
 
         delay = 0.005   # 테스트중..
@@ -63,7 +54,6 @@ class server:
         
         # 자동차 
         if (who == "[Car]"):
-<<<<<<< HEAD
             #self.carLogic(cmd, delay)
             if (cmd == self.command.FOWARD):
                 MecanumDriver.carForward(delay)
@@ -82,41 +72,13 @@ class server:
             elif(cmd == self.command.DIR11):
                 MecanumDriver.carDir11(delay)
             elif(cmd == self.command.STOP):
-=======
-
-            delay = 0.0001 # 테스트중..
-            sec = 0.5 #테스트중..
-            
-            if (cmd == command.FOWARD):
-                MecanumDriver.carForward_sec(delay,sec)
-            elif(cmd == command.REVERSE):
-                MecanumDriver.carReverse_sec(delay,sec)
-            elif(cmd == command.LEFT):
-                MecanumDriver.carLeft_sec(delay,sec)
-            elif(cmd == command.RIGHT):
-                MecanumDriver.carRight_sec(delay,sec)
-            elif(cmd == command.DIR1):
-                MecanumDriver.carDir1_sec(delay,sec)
-            elif(cmd == command.DIR5):
-                MecanumDriver.carDir5_sec(delay,sec)
-            elif(cmd == command.DIR7):
-                MecanumDriver.carDir7_sec(delay,sec)
-            elif(cmd == command.DIR11):
-                MecanumDriver.carDir11_sec(delay,sec)
-            elif(cmd == command.RIGHT_ROT):
-                MecanumDriver.rightRotate_sec(delay,sec)
-            elif(cmd == command.LEFT_ROT):
-                MecanumDriver.leftRotate_sec(delay,sec)    
-            elif(cmd == command.STOP):
->>>>>>> 1c7f4a5aeea320f75b04f971e069a4d7eeb884e1
                 MecanumDriver.carStop()
-            elif(cmd == command.TEST):
+            elif(cmd == self.command.TEST):
                 MecanumDriver.carTest()
             else:
                 MecanumDriver.carStop()
 
             outputStr = "자동차 이동명령("+cmd+") 실행"
-<<<<<<< HEAD
             self.db.insert_command_one(cmd, outputStr, self.device.MOTOR)
             
         # 펭수
@@ -131,32 +93,21 @@ class server:
                 self.ps.speakVoice(outputStr)
 
             elif (cmd == self.command.P_UP):
-=======
-            # db.insert_command_one(cmd, outputStr, device.MOTOR)
-           
-        elif (who == "[PS]"):
-            if (cmd == command.P_UP):
->>>>>>> 1c7f4a5aeea320f75b04f971e069a4d7eeb884e1
                 MecanumDriver.carForward_sec(delay, sec)
-            elif(cmd == command.P_DOWN):
+            elif(cmd == self.command.P_DOWN):
                 MecanumDriver.carReverse_sec(delay, sec)
-            elif(cmd == command.p_LEFT):
+            elif(cmd == self.command.p_LEFT):
                 MecanumDriver.carLeft_sec(delay, sec)
-            elif(cmd == command.P_RIGHT):
+            elif(cmd == self.command.P_RIGHT):
                 MecanumDriver.carRight_sec(delay, sec)
-            elif(cmd == command.P_STOP):
+            elif(cmd == self.command.P_STOP):
                 MecanumDriver.carStop()
 
-<<<<<<< HEAD
             if(outputStr != ""):
                 outputStr = "펭수 음성명령("+cmd+") 실행"
             else:
                 pass
             self.db.insert_command_one(cmd, outputStr, self.device.MOTOR)
-=======
-            outputStr = "펭수 음성명령("+cmd+") 실행"
-            # db.insert_command_one(cmd, outputStr, device.MOTOR)
->>>>>>> 1c7f4a5aeea320f75b04f971e069a4d7eeb884e1
 
         else:
             outputStr = "[미처리] 알수없는 명령("+cmd+") 실행"
@@ -176,14 +127,9 @@ class server:
                 # 받은 메시지와 클라이언트 주소 화면에 출력
                 print('[Server] Received Data : %r from %r' % (data, addr))
 
-<<<<<<< HEAD
                 # 데이터검증
                 if(self.validateData(data) == False):
                     continue
-=======
-                # 데이터검증(쓰레기제거)
-                self.validateData(data)                
->>>>>>> 1c7f4a5aeea320f75b04f971e069a4d7eeb884e1
 
                 # 분기처리
                 self.controllData(data)            
