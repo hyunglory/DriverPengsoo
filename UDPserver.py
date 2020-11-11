c17cc80db67b58b02e06f06502348532769e49a8
glory <hyunglory86@gmail.com>
2020-11-11 18:20:38 +0900

    어제 커밋으로 롤백

--- UDPserver.py
+++ UDPserver.py
@@ -2,115 +2,98 @@
 import socket
 import sys
 import MecanumDriver
-from command import *
+from command import Command
 from command import Device
 from mongodb import MongoDB
-from pengsoo import Pengsoo
 
 class server:
     
 
     def __init__(self):
-        self.command = Command()
-        self.device = Device()
-        self.db = MongoDB()
-        self.ps = Pengsoo()
-        self.voice = Voice()
-
         self.PORT = 9999
         self.BUFSIZE = 256
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server.bind(('', self.PORT))
-        print ('[Server] Pengsoo Server Ready!')        
-    # 데이터 검증
-    def validateData(self, data):
-        if (data == None):
-            print("[validateData] 비정상 데이터 - NULL")
+        print ('[Server] Pengsoo Server Ready!')
+        self.onLineServer()
+        
 
+    # 데이터 검증
+    def validateData(self, data):        
+        ret = False
+        
+        if (data != ""):
             return False
         data = data.strip()
-        return False
-
-    def carLogic(self, cmd, delay):
-        pass
-
-    def pengsooLogic(self, cmd, delay, sec):
-        pass        
+        print("")
+        return ret
 
     # 분기문
     def controllData(self, data):  
+        command = Command()
+        device = Device()
+        db = MongoDB()      
         start_index = data.find('[')
+        data = data[start_index:]
         end_index = data.find("]")
-
-        data = data[start_index:]        
         who = data[:end_index+1]
         cmd = data[end_index+1:]
-        print("who:",who)
+        print("who:", who)
         print("cmd:", cmd)
 
-        delay = 0.005   # 테스트중..
-        sec = 0.5       # 테스트중..
-        
-        # 자동차 
         if (who == "[Car]"):
-            #self.carLogic(cmd, delay)
-            if (cmd == self.command.FOWARD):
-                MecanumDriver.carForward(delay)
-            elif(cmd == self.command.REVERSE):
-                MecanumDriver.carReverse(delay)
-            elif(cmd == self.command.LEFT):
-                MecanumDriver.carLeft(delay)
-            elif(cmd == self.command.RIGHT):
-                MecanumDriver.carRight(delay)
-            elif(cmd == self.command.DIR1):
-                MecanumDriver.carDir1(delay)
-            elif(cmd == self.command.DIR5):
-                MecanumDriver.carDir5(delay)
-            elif(cmd == self.command.DIR7):
-                MecanumDriver.carDir7(delay)
-            elif(cmd == self.command.DIR11):
-                MecanumDriver.carDir11(delay)
-            elif(cmd == self.command.STOP):
+
+            delay = 0.0001 # 테스트중..
+            sec = 0.5 #테스트중..
+            
+            if (cmd == command.FOWARD):
+                MecanumDriver.carForward_sec(delay,sec)
+            elif(cmd == command.REVERSE):
+                MecanumDriver.carReverse_sec(delay,sec)
+            elif(cmd == command.LEFT):
+                MecanumDriver.carLeft_sec(delay,sec)
+            elif(cmd == command.RIGHT):
+                MecanumDriver.carRight_sec(delay,sec)
+            elif(cmd == command.DIR1):
+                MecanumDriver.carDir1_sec(delay,sec)
+            elif(cmd == command.DIR5):
+                MecanumDriver.carDir5_sec(delay,sec)
+            elif(cmd == command.DIR7):
+                MecanumDriver.carDir7_sec(delay,sec)
+            elif(cmd == command.DIR11):
+                MecanumDriver.carDir11_sec(delay,sec)
+            elif(cmd == command.RIGHT_ROT):
+                MecanumDriver.rightRotate_sec(delay,sec)
+            elif(cmd == command.LEFT_ROT):
+                MecanumDriver.leftRotate_sec(delay,sec)    
+            elif(cmd == command.STOP):
                 MecanumDriver.carStop()
-            elif(cmd == self.command.TEST):
+            elif(cmd == command.TEST):
                 MecanumDriver.carTest()
             else:
                 MecanumDriver.carStop()
 
             outputStr = "자동차 이동명령("+cmd+") 실행"
-            #self.db.insert_command_one(cmd, outputStr, self.device.MOTOR)
-            
-        # 펭수
+            # db.insert_command_one(cmd, outputStr, device.MOTOR)
+           
         elif (who == "[PS]"):
-            #self.pengsooLogic(cmd, delay, sec)
-            outputStr = ""
-            if(cmd == self.command.P_LOGIN):
-                self.ps.speakVoice(self.voice.WELCOME)
-            elif(cmd == self.command.P_SPEAK):                
-                retText = self.ps.listenVoice()
-                outputStr = "I was given a command to'"+retText+"'"
-                self.ps.speakVoice(outputStr)
-
-            elif (cmd == self.command.P_UP):
+            if (cmd == command.P_UP):
                 MecanumDriver.carForward_sec(delay, sec)
-            elif(cmd == self.command.P_DOWN):
+            elif(cmd == command.P_DOWN):
                 MecanumDriver.carReverse_sec(delay, sec)
-            elif(cmd == self.command.p_LEFT):
+            elif(cmd == command.p_LEFT):
                 MecanumDriver.carLeft_sec(delay, sec)
-            elif(cmd == self.command.P_RIGHT):
+            elif(cmd == command.P_RIGHT):
                 MecanumDriver.carRight_sec(delay, sec)
-            elif(cmd == self.command.P_STOP):
+            elif(cmd == command.P_STOP):
                 MecanumDriver.carStop()
 
-            if(outputStr != ""):
-                outputStr = "펭수 음성명령("+cmd+") 실행"
-            else:
-                pass
-            self.db.insert_command_one(cmd, outputStr, self.device.MOTOR)
+            outputStr = "펭수 음성명령("+cmd+") 실행"
+            # db.insert_command_one(cmd, outputStr, device.MOTOR)
 
         else:
             outputStr = "[미처리] 알수없는 명령("+cmd+") 실행"
-            db.insert_command_one(cmd, outputStr, device.MOTOR)
+            # db.insert_command_one(cmd, outputStr, device.MOTOR)
             print(outputStr)
 
     # 서버 통신
@@ -126,9 +109,8 @@ class server:
                 # 받은 메시지와 클라이언트 주소 화면에 출력
                 print('[Server] Received Data : %r from %r' % (data, addr))
 
-                # 데이터검증
-                if(self.validateData(data) == False):
-                    continue
+                # 데이터검증(쓰레기제거)
+                self.validateData(data)                
 
                 # 분기처리
                 self.controllData(data)            
@@ -144,6 +126,5 @@ class server:
 
 if __name__ == "__main__":
     sv = server()
-    sv.onLineServer()
 
 
