52b315aa6cd70c643c45ca13f5df164217e52113
HyungJoo <joo9437@naver.com>
2020-11-16 13:41:05 +0900

    edit avoidOBstacle.py

--- avoidObstacle.py
+++ avoidObstacle.py
@@ -4,9 +4,9 @@ import MecanumDriver
 import RPi.GPIO as GPIO
 import time
 
-FL0, FR0, LL0, LR0
-FL45, FR45, LL45, LR45
-FL90, FR90, LL90, LR90
+FL0 = FR0 = LL0 = LR0 = 0
+FL45 = FR45 = LL45 = LR45 = 0
+FL90 = FR90 = LL90 = LR90 = 0
 
 FowardBehindLimit = 10
 sideLimit = 5
@@ -25,40 +25,40 @@ def returnCarFunc(delay, sec):
     
     for i in carFunc:
         if (carFunc[i] == "carLeft"):
-            carLeft_sec(delay, sec)
+            MecanumDriver.carLeft_sec(delay, sec)
         elif (carFunc[i] == "carRight"):
-            carRight_sec(delay, sec)
+            MecanumDriver.carRight_sec(delay, sec)
         else :
-            carStop()
+            MecanumDriver.carStop()
     
     
 
 def checkFront():
-    pwmGo(0)    # 앞뒤 방향
-    FL0 = FLgetDistance()
-    FR0 = FRgetDistance()
-    LL0 = LLgetDistance()
-    LR0 = LRgetDistance()
+    servo.pwmGo(0)    # 앞뒤 방향
+    FL0 = sensor.FLgetDistance()
+    FR0 = sensor.FRgetDistance()
+    LL0 = sensor.LLgetDistance()
+    LR0 = sensor.LRgetDistance()
 
 def checkAll(): # 전체 센서 값 저장
     
-    pwmGo(0)    # 앞뒤 방향
-    FL0 = FLgetDistance()
-    FR0 = FRgetDistance()
-    LL0 = LLgetDistance()
-    LR0 = LRgetDistance()
+    servo.pwmGo(0)    # 앞뒤 방향
+    FL0 = sensor.FLgetDistance()
+    FR0 = sensor.FRgetDistance()
+    LL0 = sensor.LLgetDistance()
+    LR0 = sensor.LRgetDistance()
 
-    pwmGo(45)   # 대각선 방향
-    FL45 = FLgetDistance()
-    FR45 = FRgetDistance()
-    LL45 = LLgetDistance()
-    LR45 = LRgetDistance()
+    servo.pwmGo(45)   # 대각선 방향
+    FL45 = sensor.FLgetDistance()
+    FR45 = sensor.FRgetDistance()
+    LL45 = sensor.LLgetDistance()
+    LR45 = sensor.LRgetDistance()
 
-    pwmGo(90)   # 측면 방향
-    FL90 = FLgetDistance()   
-    FR90 = FRgetDistance()
-    LL90 = LLgetDistance()
-    LR90 = LRgetDistance()
+    servo.pwmGo(90)   # 측면 방향
+    FL90 = sensor.FLgetDistance()   
+    FR90 = sensor.FRgetDistance()
+    LL90 = sensor.LLgetDistance()
+    LR90 = sensor.LRgetDistance()
 
 def avoidMode(delay, sec):
     
@@ -67,47 +67,47 @@ def avoidMode(delay, sec):
         checkAll()
 
         if (FL0 < FowardBehindLimit or FR0 < FowardBehindLimit):
-            carStop()
+            MecanumDriver.carStop()
             checkAll()
 
             if (FL90 < sideLimit or LL90 < sideLimit):
-                carRight_sec(delay, sec)
+                MecanumDriver.carRight_sec(delay, sec)
                 carFunc.append("carRight")    
             elif (FR90 < sideLimit or LR90 < sideLimit):
-                carLeft_sec(delay, sec)
+                MecanumDriver.carLeft_sec(delay, sec)
                 carFunc.append("carLeft")
             elif (FL45 > FR45):
-                carLeft_sec(delay, sec)
+                MecanumDriver.carLeft_sec(delay, sec)
                 carFunc.append("carLeft")
             else :
-                carRight_sec(delay, sec)
+                MecanumDriver.carRight_sec(delay, sec)
                 carFunc.append("carRight") 
         else:
-            carForward_sec(delay,sec)
+            MecanumDriver.carForward_sec(delay,sec)
             checkAll()
 
             if(FL90 > sideLimit and FR90 > sideLimit and LL90 > sideLimit and LR90 > sideLimit):
-                returnCarFunc()
+                returnCarFunc(delay, sec)
                 if (carFunc[0] == "carLeft"):
-                    carLeft_sec(delay,sec)
+                    MecanumDriver.carLeft_sec(delay,sec)
                     del carFunc[0]
                 elif (carFunc[0] == "carRight"):
-                    carRight_sec(delay, sec)
+                    MecanumDriver.carRight_sec(delay, sec)
                     del carFunc[0]
                 else:
-                    carStop()
+                    MecanumDriver.carStop()
 
             else:
                 minVal = min(FL90, FR90, LL90, LR90)
                 if (minVal < sideLimit):
                     if (minVal == FL90 or minVal == LL90): 
-                        carRight_sec(delay, sec)
+                        MecanumDriver.carRight_sec(delay, sec)
                         carFunc.append("carRight")
                     elif (minVal == FR90 or minVal == LR90):
-                        carLeft_sec(delay,sec)
+                        MecanumDriver.carLeft_sec(delay,sec)
                         carFunc.append("carLeft")
                     else:
-                        carStop()
+                        MecanumDriver.carStop()
                     
                         
 if __name__ == '__main__':
@@ -115,5 +115,5 @@ if __name__ == '__main__':
         delay = 0.0005
         sec = 0.5
 
-        carForward_sec(delay, sec)
+        MecanumDriver.carForward_sec(delay, sec)
         avoidMode(delay, sec)
