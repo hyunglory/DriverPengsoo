0d716943ef8a000ace0f6c54842c324e40da03fb
glory <hyunglory86@gmail.com>
2020-11-17 12:32:27 +0900

    [펭수] 동영상 시연용 변경사항 업데이트

--- UDPserver.py
+++ UDPserver.py
@@ -18,6 +18,9 @@ class server:
         self.server.bind(('', self.PORT))
         print ('[Server] Pengsoo Server Ready!')
 
+        self.device = Device()
+        self.db = MongoDB()
+
     # 데이터 검증
     def validateData(self, data):        
         ret = False
@@ -30,86 +33,89 @@ class server:
         
         return ret
 
+    # 자동차 명령처리
+    def carFunc(self, cmd, command, delay, sec):        
+        if (cmd == command.FOWARD):
+            MecanumDriver.carForward_sec(delay,sec)                
+        elif(cmd == command.REVERSE):
+            MecanumDriver.carReverse_sec(delay,sec)
+        elif(cmd == command.LEFT):
+            MecanumDriver.carLeft_sec(delay,sec)
+        elif(cmd == command.RIGHT):
+            MecanumDriver.carRight_sec(delay,sec)
+        elif(cmd == command.DIR1):
+            MecanumDriver.carDir1_sec(delay,sec)
+        elif(cmd == command.DIR5):
+            MecanumDriver.carDir5_sec(delay,sec)
+        elif(cmd == command.DIR7):
+            MecanumDriver.carDir7_sec(delay,sec)
+        elif(cmd == command.DIR11):
+            MecanumDriver.carDir11_sec(delay,sec)
+        elif(cmd == command.RIGHT_ROT):
+            MecanumDriver.rightRotate_sec(delay,sec)
+        elif(cmd == command.LEFT_ROT):
+            MecanumDriver.leftRotate_sec(delay,sec)    
+        elif(cmd == command.STOP):
+            MecanumDriver.carStop()
+        elif(cmd == command.TEST):
+            MecanumDriver.carTest()
+        else:
+            MecanumDriver.carStop()
+
+        outputStr = "자동차 이동명령("+cmd+") 실행"
+        self.db.insert_command_one(cmd, outputStr, self.device.MOTOR)
+        
+
+    # 펭수 명령처리
+    def pengsooFunc(self, cmd, command, delay, sec):
+        voice = Voice()
+        ps = Pengsoo()
+        outputStr = ""
+
+        if(cmd == command.P_LOGIN):
+            ps.speakVoice(voice.WELCOME)
+        elif(cmd == command.P_SPEAK):
+            retText = ps.listenVoice()
+            outputStr = retText
+            ps.speakVoice(voice.WELCOME)
+        elif (cmd == command.P_UP):
+            MecanumDriver.carForward_sec(delay, sec)            
+        elif(cmd == command.P_DOWN):
+            MecanumDriver.carReverse_sec(delay, sec)
+        elif(cmd == command.p_LEFT):
+            MecanumDriver.carLeft_sec(delay, sec)
+        elif(cmd == command.P_RIGHT):
+            MecanumDriver.carRight_sec(delay, sec)
+        elif(cmd == command.P_STOP):
+            MecanumDriver.carStop()
+
+        outputStr = "펭수 음성명령("+cmd+") 실행"
+        self.db.insert_command_one(cmd, outputStr, self.device.MOTOR)
+
     # 분기문
     def controllData(self, data):  
         command = Command()
-        device = Device()
-        db = MongoDB()
-        voice = Voice()
-        ps = Pengsoo()
 
         start_index = data.find('[')
         data = data[start_index:]
         end_index = data.find("]")
         who = data[:end_index+1]
         cmd = data[end_index+1:]
-        print("who:", who)
-        print("cmd:", cmd)
+        print("who:" + who + ", cmd:" + cmd)
 
         delay = 0.0005
         sec = 0.5
         
         # 자동차 
         if (who == "[Car]"):            
-            if (cmd == command.FOWARD):
-                MecanumDriver.carForward_sec(delay,sec)
-            elif(cmd == command.REVERSE):
-                MecanumDriver.carReverse_sec(delay,sec)
-            elif(cmd == command.LEFT):
-                MecanumDriver.carLeft_sec(delay,sec)
-            elif(cmd == command.RIGHT):
-                MecanumDriver.carRight_sec(delay,sec)
-            elif(cmd == command.DIR1):
-                MecanumDriver.carDir1_sec(delay,sec)
-            elif(cmd == command.DIR5):
-                MecanumDriver.carDir5_sec(delay,sec)
-            elif(cmd == command.DIR7):
-                MecanumDriver.carDir7_sec(delay,sec)
-            elif(cmd == command.DIR11):
-                MecanumDriver.carDir11_sec(delay,sec)
-            elif(cmd == command.RIGHT_ROT):
-                MecanumDriver.rightRotate_sec(delay,sec)
-            elif(cmd == command.LEFT_ROT):
-                MecanumDriver.leftRotate_sec(delay,sec)    
-            elif(cmd == command.STOP):
-                MecanumDriver.carStop()
-            elif(cmd == command.TEST):
-                MecanumDriver.carTest()
-            else:
-                MecanumDriver.carStop()
-
-            outputStr = "자동차 이동명령("+cmd+") 실행"
-            # db.insert_command_one(cmd, outputStr, device.MOTOR)
+            self.carFunc(cmd, command, delay, sec)
            
         elif (who == "[PS]"):
-            if(cmd == command.P_LOGIN):
-                ps.speakVoice(voice.WELCOME)
-            elif(cmd == command.P_SPEAK):                
-                retText = ps.listenVoice()
-                #retText를 받고 자동차 제어 처리 필요
-                MecanumDriver.carForward_sec(delay,sec) # 테스트용으로 앞으로 가도록 구현
-
-
-                outputStr = voice.LISTEN
-                ps.speakVoice(outputStr)
-
-            elif (cmd == command.P_UP):
-                MecanumDriver.carForward_sec(delay, sec)
-            elif(cmd == command.P_DOWN):
-                MecanumDriver.carReverse_sec(delay, sec)
-            elif(cmd == command.p_LEFT):
-                MecanumDriver.carLeft_sec(delay, sec)
-            elif(cmd == command.P_RIGHT):
-                MecanumDriver.carRight_sec(delay, sec)
-            elif(cmd == command.P_STOP):
-                MecanumDriver.carStop()
-
-            outputStr = "펭수 음성명령("+cmd+") 실행"
-            # db.insert_command_one(cmd, outputStr, device.MOTOR)
+            self.pengsooFunc(cmd, command, delay, sec)
 
         else:
             outputStr = "[미처리] 알수없는 명령("+cmd+") 실행"
-            # db.insert_command_one(cmd, outputStr, device.MOTOR)
+            self.db.insert_command_one(cmd, outputStr, self.device.MOTOR)
             print(outputStr)
 
     # 서버 통신
