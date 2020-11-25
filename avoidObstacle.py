9d4d06b23d7f21e2842696159d5ecec007b5e377
Ren <>
2020-11-25 09:32:25 +0900

    물체 피하기 및 초음파센서 측정범위 최대값 500 제한

--- avoidObstacle.py
+++ avoidObstacle.py
@@ -14,222 +14,162 @@ FrontLimit = 20
 diagonalLimit = 20
 sideLimit = 20
 sleep=1.5
-
+cycle=2
 
 carFunc = []
 
-def returnCarFunc(delay, sec):
-    if(not carFunc):
+def returnCarFunc():
+    
+    if(len(carFunc)==0):
         return True
+    print("리턴 시작")
+    # for i in range(len(carFunc)):
+    #     if (carFunc[i] == "[Car]Right"):
+    #         carFunc[i] = "[Car]Left"
+    #     elif (carFunc[i] == "[Car]Left"):
+    #         carFunc[i] = "[Car]Right"
+    #     elif (carFunc[i] == "[Car]Dir1"):
+    #         carFunc[i] = "[Car]Dir11"
+    #     elif (carFunc[i] == "[Car]Dir11"):
+    #         carFunc[i] = "[Car]Dir1"
 
     for i in range(len(carFunc)):
-        if (carFunc[i] == "carLeft"):
-            carFunc[i] == "carRight"
-        elif (carFunc[i] == "carRight"):
-            carFunc[i] == "carLeft"
-        else :
-            carFunc[i] == "wrong value"
-    
-    # for i in range(len(carFunc)):
-    #     if (carFunc[i] == "carLeft"):
-    #         client("[Car]Left")
-    #     elif (carFunc[i] == "carRight"):
-    #         client("[Car]Right")
-    #     else :
-    #         client("[Car]Stop")
-    if (carFunc[0] == "carLeft"):
-        client("[Car]Left")
-        del carFunc[0]
-    elif (carFunc[i] == "carRight"):
-        client("[Car]Right")
+        if (carFunc[i] == "[Car]Right"):
+            client("[Car]Right")
+            time.sleep(sleep)
+        elif (carFunc[i] == "[Car]Left"):
+            client("[Car]Left")
+            time.sleep(sleep)
+        elif (carFunc[i] == "[Car]Dir1"):
+            client("[Car]Dir1")
+            time.sleep(sleep)
+        elif (carFunc[i] == "[Car]Dir11"):
+            client("[Car]Dir11")
+            time.sleep(sleep)
+        elif (carFunc[i] == "[Car]Forward"):
+            client("[Car]Forward")
+            time.sleep(sleep)
+
+    for i in range(len(carFunc)):
+        print("{} 삭제".format(carFunc[0]))
         del carFunc[0]
 
+    print("리턴 종료")   
 
 def checkAll(): # 전체 센서 값 저장
     global FL0, FR0, LL0, LR0 
     global FL45, FR45, LL45, LR45
     global FL90, FR90, LL90, LR90  
 
-        
     FL0 = sensor.FLgetDistance()
     FR0 = sensor.FRgetDistance()
     #LL0 = sensor.LLgetDistance()
     #LR0 = sensor.LRgetDistance()
-    print("0도 " + "FL: " + str(FL0) + " FR " + str(FR0))
+    print("[회피]0도 " + "FL: " + str(FL0) + " FR " + str(FR0))
     
-
     servo.pwmGo(55)   # 대각선 방향
     time.sleep(0.8)
     FL45 = sensor.FLgetDistance()
     FR45 = sensor.FRgetDistance()
     #LL45 = sensor.LLgetDistance()
     #LR45 = sensor.LRgetDistance()
-    print("45도 " + "FL: " + str(FL45) + " FR " + str(FR45))
+    print("[회피]45도 " + "FL: " + str(FL45) + " FR " + str(FR45))
     
-
     servo.pwmGo(110)   # 측면 방향
     time.sleep(0.8)
     FL90 = sensor.FLgetDistance()   
     FR90 = sensor.FRgetDistance()
     LL90 = sensor.LLgetDistance()
     LR90 = sensor.LRgetDistance()
-    print("90도 " + "FL: " + str(FL90) + " FR " + str(FR90)+ " LL " + str(LL90)+ " LR "+ str(LR90))
+    print("[회피]90도 " + "FL: " + str(FL90) + " FR " + str(FR90)+ " LL " + str(LL90)+ " LR "+ str(LR90))
     servo.pwmGo(0)  # 앞뒤 방향
     
-
 def avoidMode(sleep):
     checkAll()
-    if (FL90 < sideLimit):
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-
-    elif (FL45 < diagonalLimit):
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Forward")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Forward")
-        time.sleep(sleep)
-
-    elif (FL0 < FrontLimit):
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Dir5")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Dir5")
-        time.sleep(sleep)
-
-    elif (FR0 < FrontLimit):
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Dir7")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Dir7")
-        time.sleep(sleep)
-
-    elif (FR45 < diagonalLimit):
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Forward")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Forward")
-        time.sleep(sleep)
-
-    elif (FR90 < sideLimit):
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-
-    elif (LL90 < sideLimit):
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-        client("[Car]Right")
-        time.sleep(sleep)
-
-    elif (LR90 < sideLimit):
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-        client("[Car]Left")
-        time.sleep(sleep)
-
-    elif(max(FL90, FL45,FL0, FR0, FR45, FR90, LL90, LR90) >= FrontLimit) :
-        print("-------------------------------------------------------")
-        print("리미트 범위 초과")
-        print("-------------------------------------------------------")
-        client("[Car]Forward")
-        time.sleep(sleep)
-        maxVal = max(FL90, FL45,FL0, FR0, FR45, FR90, LL90, LR90)
-        if (maxVal == FL90):
-            client("[Car]Left")
-            time.sleep(sleep)
-            client("[Car]Left")
-            time.sleep(sleep)
-
-        elif (maxVal == FL45):
-            client("[Car]Dir11")
-            time.sleep(sleep)
-            client("[Car]Dir11")
-            time.sleep(sleep)
-
-        elif (maxVal == FL0):
-            client("[Car]Forward")
-            time.sleep(sleep)
-            client("[Car]Forward")
-            time.sleep(sleep)
-
-        elif (maxVal == FR0):
-            client("[Car]Forward")
-            time.sleep(sleep)
-            client("[Car]Forward")
-            time.sleep(sleep)
-
-        elif (maxVal == FR45):
-            client("[Car]Dir1")
-            time.sleep(sleep)
-            client("[Car]Dir1")
-            time.sleep(sleep)
-
-        elif (maxVal == FR90):
+    if (FL90 < sideLimit and sum([FL0,FL45,FL90,LL90]) < sum([FR0,FR45,FR90,LR90])):
+        for i in range(cycle):
             client("[Car]Right")
+            carFunc.append("[Car]Left")
             time.sleep(sleep)
             client("[Car]Right")
+            carFunc.append("[Car]Left")
             time.sleep(sleep)
 
-        elif (maxVal == LL90):
+    elif (FL45 < diagonalLimit and sum([FL0,FL45,FL90,LL90]) < sum([FR0,FR45,FR90,LR90])):
+        for i in range(cycle):
+            client("[Car]Right")
+            carFunc.append("[Car]Left")
+            time.sleep(sleep)
+            client("[Car]Forward")
+            carFunc.append("[Car]Forward")
+            time.sleep(sleep)
+
+    elif (FL0 < FrontLimit and sum([FL0,FL45,FL90,LL90]) < sum([FR0,FR45,FR90,LR90])):
+        for i in range(cycle):
+            client("[Car]Right")
+            carFunc.append("[Car]Left")
+            time.sleep(sleep)
+            client("[Car]Dir1")
+            carFunc.append("[Car]Dir11")
+            time.sleep(sleep)
+
+    elif (FR0 < FrontLimit and sum([FR0,FR45,FR90,LR90]) < sum([FL0,FL45,FL90,LL90])):
+        for i in range(cycle):
             client("[Car]Left")
+            carFunc.append("[Car]Right")
+            time.sleep(sleep)
+            client("[Car]Dir11")
+            carFunc.append("[Car]Dir1")
+            time.sleep(sleep)
+
+    elif (FR45 < diagonalLimit and sum([FR0,FR45,FR90,LR90]) < sum([FL0,FL45,FL90,LL90])):
+        for i in range(cycle):
+            client("[Car]Left")
+            carFunc.append("[Car]Right")
+            time.sleep(sleep)
+            client("[Car]Forward")
+            carFunc.append("[Car]Forward")
+            time.sleep(sleep)
+
+    elif (FR90 < sideLimit and sum([FR0,FR45,FR90,LR90]) < sum([FL0,FL45,FL90,LL90])):
+        for i in range(cycle):
+            client("[Car]Left")
+            carFunc.append("[Car]Right")
             time.sleep(sleep)
             client("[Car]Left")
+            carFunc.append("[Car]Right")
             time.sleep(sleep)
+
+    elif (LL90 < sideLimit and sum([FL0,FL45,FL90,LL90]) < sum([FR0,FR45,FR90,LR90])):
+        for i in range(cycle):
+            client("[Car]Right")
+            carFunc.append("[Car]Left")
+            time.sleep(sleep)
+            client("[Car]Right")
+            carFunc.append("[Car]Left")
+            time.sleep(sleep)
+
+    elif (LR90 < sideLimit and sum([FR0,FR45,FR90,LR90]) < sum([FL0,FL45,FL90,LL90])):
+        for i in range(cycle):
+            client("[Car]Left")
+            carFunc.append("[Car]Right")
+            time.sleep(sleep)
+            client("[Car]Left")
+            carFunc.append("[Car]Right")
+            time.sleep(sleep)
+
+    elif(FL90+FL45+FL0+FR0+FR45+FR90+LL90+LR90 <= FrontLimit*8) :
+        print("-------------------------------------------------------")
+        print("탈출 모드")
+        print("-------------------------------------------------------")
+        #maxVal = max(FL90, FL45,FL0, FR0, FR45, FR90, LL90, LR90)
         
-        elif (maxVal == LR90):
-            client("[Car]Right")
-            time.sleep(sleep)
-            client("[Car]Right")
-            time.sleep(sleep)
-        else:
-            print("예외값 오류2")
-
     else:
         print("예외값 오류1")
-                
-                
-        
-
-            
-
-        
-                    
                         
 def client(data):
 
-    host = "192.168.0.55" # 라파4: 55 / 라파3 139
+    host = "192.168.0.43" # 라파4: 43 / 라파3 139
     port = 9999
     data = data.encode()
 
@@ -252,16 +192,21 @@ if __name__ == '__main__':
         servo.pwmGo(0)
         FL0 = sensor.FLgetDistance()
         FR0 = sensor.FRgetDistance()
-        print("0도 " + "FL: " + str(FL0) + " FR " + str(FR0))
+        LL90 = sensor.FLgetDistance()
+        LR90 = sensor.FRgetDistance()
+        print("[앞으로]0도 " + "FL: " + str(FL0) + " FR: " + str(FR0))
+        print("[앞으로]90도 "+ "LL: " + str(LL90)+ " LR: "+ str(LR90))
         if(FL0 < FrontLimit or FR0 < FrontLimit):
             avoidMode(sleep)
         else :
             client("[Car]Forward")
             time.sleep(sleep)
+            #if(len(carFunc)>0 and LR90 > 300 and LL90 > 300):
+            for i in range(int(len(carFunc)*2)):
+                client("[Car]Forward")
+                time.sleep(sleep)
 
-
-            
-        
+            returnCarFunc()
 
 # go(목적지)
 # avoidMode()를 반복해야 할듯
--- sensor.py
+++ sensor.py
@@ -13,8 +13,6 @@ LLTRIG = 19
 LLECHO = 26
 LRTRIG = 20
 LRECHO = 21
-MAPTRIG = 
-MAPECHO = 
 
 GPIO.setup(FLTRIG, GPIO.OUT)
 GPIO.setup(FLECHO, GPIO.IN)
@@ -24,29 +22,8 @@ GPIO.setup(LLTRIG, GPIO.OUT)
 GPIO.setup(LLECHO, GPIO.IN)
 GPIO.setup(LRTRIG, GPIO.OUT)
 GPIO.setup(LRECHO, GPIO.IN)
-GPIO.setup(MAPTRIG, GPIO.OUT)
-GPIO.setup(MAPECHO, GPIO.IN)
-
 
 GPIO.setwarnings(False)
-
-def MAPgetDistance():
-    GPIO.output(MAPTRIG, False)
-    time.sleep(0.0001)
-    GPIO.output(MAPTRIG, True)
-    time.sleep(0.0001)
-    GPIO.output(MAPTRIG, False)
-    
-    while GPIO.input(MAPECHO) == 0:
-        pulse_start = time.time()
-        
-    while GPIO.input(MAPECHO) == 1:
-        pulse_end = time.time()
-        
-    pulse_duration = pulse_end-pulse_start
-    distance = round(pulse_duration * 17150, 2)
-        
-    return distance
 
 def FLgetDistance():
     
@@ -66,7 +43,10 @@ def FLgetDistance():
 
     pulse_duration = pulse_end-pulse_start
     distance = round(pulse_duration * 17150, 2)
-        
+
+    if (distance > 500):
+        distance = 500
+
     return distance
 
 def FRgetDistance():
@@ -84,6 +64,9 @@ def FRgetDistance():
 
     pulse_duration = pulse_end-pulse_start
     distance = round(pulse_duration * 17150, 2)
+
+    if (distance > 500):
+        distance = 500
         
     return distance
 
@@ -103,6 +86,9 @@ def LLgetDistance():
 
     pulse_duration = pulse_end-pulse_start
     distance = round(pulse_duration * 17150, 2)
+
+    if (distance > 500):
+        distance = 500
         
     return distance
 
@@ -121,6 +107,9 @@ def LRgetDistance():
 
     pulse_duration = pulse_end-pulse_start
     distance = round(pulse_duration * 17150, 2)
+
+    if (distance > 500):
+        distance = 500
         
     return distance
 
